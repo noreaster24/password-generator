bdca98e45a10fda8c46897768ae7c92944ba948b
Joe Porrazzo Jr <joeporrazzojr@joes-mac-mini.lan>
2021-04-03 13:15:15 -0700

    randomCharacters function and assiging characters

--- Assets/script.js
+++ Assets/script.js
@@ -1,23 +1,62 @@
-// Assignment code here
-var length = Number(prompt("How many characters would you like your password to contain?")); {
-  if (length < 8 || length < 128) {
-    alert("Your password must contain between 8 and 128 characters");
-    return false;
+// Generate the password
+function generatePassword() {
+  debugger;
+  var passwordLength = prompt("How many characters would you like your password to be?");
+  console.log(passwordLength);
+  // Generate random characters
+  function randomCharacters() {
+    if (useUpperCase === true) {
+      getUpperCase();
+    }
+    if (useLowerCase === true) {
+      getLowerCase();
+    }
+    if (useNumberChar === true) {
+      getNumbers();
+    }
+    if (useSymbolChar === true) {
+      getSymbol()
+    }
   }
+  if (passwordLength > 7 && passwordLength < 129) {
+    for (var i = 0; i < passwordLength.length; i++) {
+      randomCharacters();
+    }
+    return randomCharacters;
+  }
+  else {
+    alert("You need to choose a password that is between 8 and 128 characters");
+  }
+};
+
+// Is this needed?
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowerCase = "abcdefghijklmnopqrstuvwyz";
+var numberChar = "0123456789";
+var symbolChar = "!@#$%^&*()_+=-/.,?><:;"
+var characterSet = "";
+
+// Ask the user which character types they'd like to use
+var useUpperCase = confirm("Would you like to use uppercase letters?");
+var useLowerCase = confirm("Would you like to use lowercase letters?");
+var useNumberChar = confirm("Would you like to use numbers?");
+var useSymbolChar = confirm("Would you like to use special symbols?");
+
+
+// Generate character functions
+function getUpperCase() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+function getLowerCase() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+function getNumbers() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 48);
+}
+function getSymbol() {
+  return String.fromCharCode(Math.floor(Math.random()));
 }
 
-var uppers = confirm("Would you like to use uppercase letters?");
-var lowers = confirm("Would you like to use lowercase letters?");
-var numbers = confirm("Would you like to use numbers?");
-var symbols = confirm("Would you like to use special characters?");
-
-while (!uppers && !lowers && !numbers && !symbols) {
-  alert("You must select at least one character type!");
-  var uppers = confirm("Would you like to use uppercase letters?");
-  var lowers = confirm("Would you like to use lowercase letters?");
-  var numbers = confirm("Would you like to use numbers?");
-  var symbols = confirm("Would you like to use special characters?");
-}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
